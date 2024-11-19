b195cd4759d9737f728d624dc24420633ef7ca04
Elias Neto <netoeac.dev@gmail.com>
2024-11-19 20:57:45 -0300

    update meta data

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Portal Administrativo - SUED",
-  description: "Ficha técnica!",
+  title: "Portal Administrativo - SUED Governo",
+  description: "Fichas técnicas para merenda escolar",
 };
 
 export default function Layout({
